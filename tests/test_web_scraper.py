7d22c193c2d5be8864121407243004e1e06834b6
Aaron Uurman <aaronuurman@gmail.com>
2019-11-22 22:45:40 +0200

    Fix image download test

--- tests/test_web_scraper.py
+++ tests/test_web_scraper.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 
 import unittest
-from assistant.commands.blog import web_scraper
 import tempfile, shutil, os
+from assistant.commands.blog import web_scraper
+from assistant.commands.blog.download_img import DownloadImg
 
 class TestWebScraper(unittest.TestCase):
+  @classmethod
+  def setUpClass(cls):
+    os.mkdir('/tmp/blog')
+    os.mkdir('/tmp/blog/src')
+    os.mkdir('/tmp/blog/src/images')
 
   def test_unsplash_url_download_img_success(self):
     #Arrange
-    image_path =  '/'.join(('/tmp', 'tempImg.jpg'))
+    project_path = '/tmp/blog'
+    image_url = 'https://unsplash.com/photos/AhC4pduPcGU'
+    title = 'Temporary Testing'
+    downloadTask = DownloadImg(project_path, image_url, title)
     #Act
-    web_scraper.download_img('https://unsplash.com/photos/AhC4pduPcGU', image_path)
+    downloadTask.execute()
     #Assert
-    self.assertTrue(os.path.isfile(image_path))
-    #CleanUp
-    os.remove(image_path)
+    self.assertTrue(os.path.isfile('/tmp/blog/src/images/temporary-testing.jpg'))
 
   def test_unsplash_url_get_image_author_success(self):
     #Arrange
@@ -29,6 +36,10 @@ class TestWebScraper(unittest.TestCase):
     self.assertEqual(image.author_profile, author_profile)
     self.assertEqual(image.file_name, image_file)
     self.assertEqual(image.url, image_url)
+
+  @classmethod
+  def tearDownClass(cls):
+    shutil.rmtree('/tmp/blog')
 
 if __name__ == '__main__':
   unittest.main()
